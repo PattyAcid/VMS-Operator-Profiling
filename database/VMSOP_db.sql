dedd628eefb11a17d439878d26d03fa5fd047aca
PattyAcid <alfasajailaniabduraup@gmail.com>
2024-03-01 09:34:33 +0800

    asasas

--- database/VMSOP_db.sql
+++ database/VMSOP_db.sql
@@ -3,17 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 29, 2024 at 08:23 AM
+-- Generation Time: Mar 01, 2024 at 01:33 AM
 -- Server version: 5.1.32
 -- PHP Version: 5.2.9-1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `startup_db`
@@ -38,17 +32,19 @@ CREATE TABLE IF NOT EXISTS `list_records` (
   `current_address` varchar(100) NOT NULL,
   `degree` varchar(100) NOT NULL,
   `date_of_hired` date NOT NULL,
+  `contact_number` bigint(20) NOT NULL,
+  `job_designation` varchar(100) NOT NULL,
+  `job_description` varchar(100) NOT NULL,
+  `job_area` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `list_records`
 --
 
-INSERT INTO `list_records` (`id`, `name_id`, `name`, `gender`, `date_of_birth`, `place_of_birth`, `civil_status`, `religion`, `tribe`, `current_address`, `degree`, `date_of_hired`) VALUES
-(1, 1, 'Alfasa J. Abduraup', 'male', '2024-02-12', 'Chinese Pier, Jolo Sulu', 'Single', 'Islam', 'Tausog', 'Recodo, Zamboanga City', 'Bachelor of Science in Information Technology', '2020-01-01'),
-(2, 0, 'Joseph L. Harun', 'Male', '1989-05-06', 'Pandan, Antique', 'Single', 'Islam', 'Sama Bangingi', 'Ayala, Zamboanga City', 'Bachelor of Science in Information Technology', '2023-02-23'),
-(3, 0, 'Mohammad A. Joaquino', 'Male', '1987-03-13', 'Mercedez, Zamboanga City', 'Married', 'Islam', 'Sama Bangingi', 'Mercedez, Zamboanga City', 'Bachelor of Science in Aquaculture', '2022-05-30');
+INSERT INTO `list_records` (`id`, `name_id`, `name`, `gender`, `date_of_birth`, `place_of_birth`, `civil_status`, `religion`, `tribe`, `current_address`, `degree`, `date_of_hired`, `contact_number`, `job_designation`, `job_description`, `job_area`) VALUES
+(1, 9898837, 'sample sample', 'lgbtq++', '2024-02-12', 'Seoul, Korea', 'Single', 'Out of this world', 'Alien', 'Recodo, Zamboanga City', 'Bachelor of Science in Information Technology', '2020-01-31', 9284567332, 'vms operator', 'operating and monitoring of VMS', 'sangali, zamboanga city');
 
 -- --------------------------------------------------------
 
