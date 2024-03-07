6bacdd4b3f720cf75b43dfdec2de853b1dacea5e
PattyAcid <alfasajailaniabduraup@gmail.com>
2024-03-07 08:26:55 +0800

    ggggggggg

--- database/vms_db.sql
+++ database/vms_db.sql
@@ -70,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `users`
